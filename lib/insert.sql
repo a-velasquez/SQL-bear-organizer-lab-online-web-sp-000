ab4b00d01109e5538c9abeb686f72ff54e43b562
Alex Velasquez <jtinnesco@gmail.com>
2019-11-27 00:27:48 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,7 +1,7 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Brownie', 3, 'Female', 'Brown', 'Serious', 1)
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 8, 'Male', 'Black', 'Grumpy', 1)
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('')
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Jack', 4, 'Male', 'Brown', 'Energetic', 1)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Molly', 6, 'Female', 'Black', 'Playful', 1)
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
